2bbb699dd7e9313bece87b6f9aee989e1290f161
OtherEdward <44999847+OtherEdward@users.noreply.github.com>
2026-01-25 15:18:10 +0000

    Update E_OPMSMS_FitSpotJunctions.m
    Finalized fitting and plotting results.

--- E_OPMSMS_FitSpotJunctions.m
+++ E_OPMSMS_FitSpotJunctions.m
@@ -13,8 +13,8 @@ load([rPath, rFile], 'rotXYZdata', 'regionIDX', 'sjIDX', 'sjData')
 
 %% get spot junction data
 
-% get junction only data
-junXYZdata = rotXYZdata(regionIDX == 4, :);
+% get spot junction only data
+sjXYZdata = rotXYZdata(regionIDX == 4, :);
 
 % get number of spot junctions
 nSJs = size(sjData, 1);
@@ -31,7 +31,22 @@ pxPad = 0.3;
 tiledlayout(1, 2)
 
 % array to store fit results
-sjFitsResults = zeros(nSJs, 11);
+sjFitsResults = zeros(nSJs, 13);
+
+% strcture with column type
+sjC.amp1 = 1;
+sjC.amp2 = 2;
+sjC.x1 = 3;
+sjC.y1 = 4;
+sjC.x2 = 5;
+sjC.y2 = 6;
+sjC.p1wX = 7;
+sjC.p1wY = 8;
+sjC.p2wX = 9;
+sjC.p2wY = 10;
+sjC.offset = 11;
+sjC.badFit = 12;
+sjC.meanZ = 13;
 
 for i = 1:nSJs
 
@@ -44,10 +59,10 @@ for i = 1:nSJs
     thisSJ = sjIDX == i;
 
     % points for this spot junction
-    sjPts = junXYZdata(thisSJ, 1:3);
+    sjPts = sjXYZdata(thisSJ, 1:3);
 
     % average z position of this junction
-    sjMeanZ = mean(sjPts(:, 3));
+    sjFitsResults(i, sjC.meanZ) = mean(sjPts(:, 3));
 
     %----------------------------------------------------------------------
     % get image data and setup for fitting
@@ -75,7 +90,7 @@ for i = 1:nSJs
     [fitR, xyMesh] = fitGaussian_2D2PeakSigmaXY(xy, b0);
 
     % store fit results
-    sjFitsResults(i, :) = fitR;
+    sjFitsResults(i, 1:11) = fitR;
     
     % make fit image
     fitIM = Gaussian_2D2PeakSigmaXY(fitR, xyMesh);
@@ -91,6 +106,98 @@ for i = 1:nSJs
     nexttile(2)
     imagesc(fitIM); axis image; colormap gray
 
+    answer = questdlg('Is the fit result good?', 'Fit Result', 'Yes','No','Yes');
+
+    switch answer
+        case 'Yes'
+            sjFitsResults(i, sjC.badFit) = false;
+        case 'No'
+            sjFitsResults(i, sjC.badFit) = true;
+    end
+
 end
 
 clear i t xG yG fitIM fitR xyMesh b0
+
+%% get spacing and position data
+
+% range for getting junction z position
+thetaRange = 3*pi/180;
+
+% mean z position vs spacing from fit
+sjSZdata = zeros(nSJs, 3);
+
+% get junction only data
+junXYZdata = rotXYZdata(regionIDX == 3, :);
+
+% convert junction data to polar
+[jTheta, jRho, jZ] = cart2pol(junXYZdata(:, 1), junXYZdata(:, 2), junXYZdata(:, 3));
+
+for i = 1:nSJs
+
+    if sjFitsResults(i, sjC.badFit)
+        % do nothing
+    else
+
+        %------------------------------------------------------------------
+        % raw sj xyz data
+
+        % logical for this spot junction
+        thisSJ = sjIDX == i;
+
+        % points for this spot junction
+        sjPts = sjXYZdata(thisSJ, 1:3);
+
+        % convert to polar
+        [sjTheta, ~, ~] = cart2pol(sjPts(:, 1), sjPts(:, 2), sjPts(:, 3));
+
+        % get the average theta for this spot junction
+        meanSJtheta = mean(sjTheta);
+
+        %------------------------------------------------------------------
+        % junction data
+
+        % get index for points in the theta range
+        jIDX = jTheta <= meanSJtheta + thetaRange & jTheta >= meanSJtheta - thetaRange;
+
+        % get xyz data for function in this theta range
+        jAbove = junXYZdata(jIDX, :);
+
+        %------------------------------------------------------------------
+        % get relative spot junction z position
+
+        rZ = abs(mean(jAbove(:, 3)) - mean(sjPts(:, 3)));
+
+        fprintf(1, '%.2f\n', rZ);
+
+        %------------------------------------------------------------------
+        % spot junction fit data
+
+        % get and store the average absolute z position for this spot junction
+        sjSZdata(i, 1) = sjFitsResults(i, sjC.meanZ);
+
+        % store relative z position
+        sjSZdata(i, 2) = rZ;
+
+        % get and store the separation between spot junctions
+        sjSZdata(i, 3) = abs(sjFitsResults(i, sjC.x1) - sjFitsResults(i, sjC.x2));
+
+        %------------------------------------------------------------------
+        % plot data
+
+        scatter3(junXYZdata(:, 1), junXYZdata(:, 2), junXYZdata(:, 3), '.')
+        axis image
+        hold on
+        scatter3(jAbove(:, 1), jAbove(:, 2), jAbove(:, 3), '.')
+        scatter3(sjPts(:, 1), sjPts(:, 2), sjPts(:, 3), '.')
+        hold off
+        view(0, 0)
+
+        pause
+    end
+
+end
+
+%% save results
+
+save([rPath, rFile], 'sjFitsResults', 'sjSZdata', 'sjC', '-append')
